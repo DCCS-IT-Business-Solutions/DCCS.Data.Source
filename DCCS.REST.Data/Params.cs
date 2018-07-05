8dd9521f7c24b557b67fa9ee3c120c0bc2fcdd95
Mario te Best <mario.tebest@dccs.at>
2018-07-05 11:13:59 +0200

    init, moved to git

---
+++ DCCS.REST.Data/Params.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCCS.REST.Data
+{
+    public class Params
+    {
+        public Params()
+        {
+        }
+
+        public Params(Params ps)
+        {
+            if (ps != null)
+            {
+                this.Page = ps.Page;
+                this.Count = ps.Count;
+                this.OrderBy = ps.OrderBy;
+                this.Desc = ps.Desc;
+            }
+        }
+
+        public int? Page { get; set; }
+        public int? Count { get; set; }
+        public string OrderBy { get; set; }
+        public bool Desc { get; set; }
+
+
+    }
+}
---
+++ DCCS.REST.Data/Result.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace DCCS.REST.Data
+{
+    public class Result<T> : Params
+    {
+        public IEnumerable<T> Data { get; protected set; }
+        public int Total { get; set; }
+
+        public Result(Params ps) : base(ps)
+        { }
+
+        public Result(Params ps, IQueryable<T> data) : base(ps)
+        {
+            SetData(data);
+        }
+
+        public void SetData(IQueryable<T> data)
+        {
+            var result = data;
+
+            // Filtern...
+            //if (Query != null) {
+            //    foreach (var prop in Query.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)) {
+            //        string query = string.Format("{0}.StartsWith(\"{1}\")", prop.Name, prop.GetValue(Query, null));
+            //        result = result.Where(query);
+            //    }
+            //}
+            //if (!string.IsNullOrWhiteSpace(Query)) {
+
+            //    result = result.Where(Query);
+            //}
+
+            // Sortieren...
+            if (!string.IsNullOrWhiteSpace(OrderBy))
+            {
+                //System.Linq.Dynamic.Core.DynamicQueryableExtensions.OrderBy()
+                result = result.OrderBy($"{OrderBy} {(Desc ? "desc" : "")}");
+            }
+            else if (data.Expression.Type != typeof(IOrderedQueryable<T>))
+            {
+                // Vor einem Skip (siehe unten) muß ein OrderBy aufgerufen werden.
+                // Ist keine Sortierung angegeben, müssen wir dennoch sortieren und behalten
+                // dabei die Reihenfolge bei.
+                result = result.OrderBy(x => true);
+            }
+
+            Total = result.Count();
+            if (Count.HasValue)
+            {
+                Count = Math.Min(Count.Value, Total);
+            }
+
+
+            // Pagen...
+            IQueryable<T> tempresult = null; // Wird für "Kann diese Seite überhaupt angezeigt werden" benötigt
+            if (Page.HasValue)
+            {
+                if (!Count.HasValue) throw new ArgumentNullException("Bei angegebener Seite (page) muss auch die Anzahl der Einträge (count) angegeben werden!");
+
+                //Manuel 24.10.2016 
+                //INFO: es wurde falsche ergebnisse geliefert weil "OrderBy>true" gefehlt hat. -> das sollte sich stephan nochmal anschauen
+                //Folgende anpassungen sind als Quickfix anzusehen und sollten noch optimiert werden
+
+                var skip = (Page.Value - 1) * Count.Value;
+                var take = Count.Value;
+
+                //Ohne OrderBy, da es eine Sortierung gibt
+                if (!string.IsNullOrWhiteSpace(OrderBy))
+                {
+                    tempresult = result.Skip(skip).Take(take);
+                }
+                //Mit OrderBy, da es keine Sortierung gibt
+                else
+                {
+                    tempresult = result.Skip(skip).OrderBy(x => true).Take(take).OrderBy(x => true);
+                }
+                if (!tempresult.Any())
+                {
+                    Page = 1;
+                    tempresult = result.Take(Count.Value);
+                }
+            }
+
+            Data = tempresult.ToArray();
+        }
+    }
+}
