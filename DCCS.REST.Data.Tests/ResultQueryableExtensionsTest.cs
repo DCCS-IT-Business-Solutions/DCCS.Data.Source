f6f45838e0abfa704a89489c9cb74455797ea8a4
smeissner <stephan.meissner@dccs.at>
2018-12-11 21:25:16 +0100

    IQueryable extension methods

---
+++ DCCS.REST.Data.Tests/ResultQueryableExtensionsTest.cs
@@ -0,0 +1,38 @@
+using Bogus;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DCCS.REST.Data.Tests
+{
+
+    [TestFixture]
+    public class ResultQueryableExtensionsTest
+    {
+        class Dummy
+        {
+            public string Name { get; set; }
+        }
+
+        class DummyDTO
+        {
+            public string Name { get; set; }
+            public int Length { get; set; }
+        }
+        public ResultQueryableExtensionsTest()
+        {
+            Randomizer.Seed = new Random(876543);
+        }
+
+        [Test]
+        public void Should_create_Result()
+        {
+            var total = 100;
+            var data = new Faker<Dummy>().Generate(total);
+            var sut = data.AsQueryable().ToResult(new Params());
+
+            Assert.AreEqual(total, sut.Total);
+        }
+    }
+}
---
+++ DCCS.REST.Data/ResultQueryableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+
+namespace DCCS.REST.Data
+{
+    public static class ResultQueryableExtensions
+    {
+        public static Result<T> ToResult<T>(this IQueryable<T> source, Params ps)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+            return new Result<T>(ps, source);
+        }
+    }
+}
