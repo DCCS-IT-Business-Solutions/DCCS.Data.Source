91c6624d16224c672d99b46abc811ba6aef72fdf
smeissner <stephan.meissner@dccs.at>
2018-12-12 11:55:30 +0100

    another one

--- Build.ps1
+++ Build.ps1
@@ -53,8 +53,7 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 EnsurePsbuildInstalled
 
 exec { & dotnet restore }
-
-Invoke-MSBuild
+exec { & dotnet build -c Release }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
